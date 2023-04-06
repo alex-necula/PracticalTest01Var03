f82d29ec387b664066016f16fca424394a651d80
Alex Necula <anecula@adobe.com>
2023-04-06 08:38:46 +0300

    Implement B

--- app/src/main/java/lab03/eim/systems/cs/pub/ro/practicaltest01var03/PracticalTest01Var03MainActivity.java
+++ app/src/main/java/lab03/eim/systems/cs/pub/ro/practicaltest01var03/PracticalTest01Var03MainActivity.java
@@ -1,17 +1,77 @@
 package lab03.eim.systems.cs.pub.ro.practicaltest01var03;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PracticalTest01Var03MainActivity extends AppCompatActivity {
+    public static final String FIRST_NUMBER_EDIT_TEXT = "firstNumberEditText";
+    public static final String SECOND_NUMBER_EDIT_TEXT = "secondNumberEditText";
+    public static final String RESULT_TEXT_VIEW = "resultTextView";
+    private EditText firstNumberEditText, secondNumberEditText;
+    private Button additionButton, subtractionButton, navigateButton;
+    private TextView resultTextView;
+    private String firstNumber, secondNumber, result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var03);
+
+        firstNumberEditText = findViewById(R.id.firstNumberEditText);
+        secondNumberEditText = findViewById(R.id.secondNumberEditText);
+        additionButton = findViewById(R.id.additionButton);
+        subtractionButton = findViewById(R.id.subtractionButton);
+        resultTextView = findViewById(R.id.resultTextView);
+        navigateButton = findViewById(R.id.navigateButton);
+
+        additionButton.setOnClickListener(view -> {
+            if (!firstNumberEditText.getText().toString().isEmpty() && !secondNumberEditText.getText().toString().isEmpty()) {
+                int firstNumber = Integer.parseInt(firstNumberEditText.getText().toString());
+                int secondNumber = Integer.parseInt(secondNumberEditText.getText().toString());
+                int result = firstNumber + secondNumber;
+                resultTextView.setText(firstNumber + " + " + secondNumber + " = " + result);
+            } else {
+                Toast.makeText(PracticalTest01Var03MainActivity.this, "Both numbers should be set.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        subtractionButton.setOnClickListener(view -> {
+            if (!firstNumberEditText.getText().toString().isEmpty() && !secondNumberEditText.getText().toString().isEmpty()) {
+                int firstNumber = Integer.parseInt(firstNumberEditText.getText().toString());
+                int secondNumber = Integer.parseInt(secondNumberEditText.getText().toString());
+                int result = firstNumber - secondNumber;
+                resultTextView.setText(firstNumber + " - " + secondNumber + " = " + result);
+            } else {
+                Toast.makeText(PracticalTest01Var03MainActivity.this, "Both numbers should be set.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    // Sa se asocieze un ascultator pentru cele 2 butoane, astfel incat la apasarea oricaruia in campul text needitabil sa se afiseze operatia care se doreste a fi realizata, impreuna cu rezultatul sau. In situatia in care oricare dintre cele 2 campuri tex editabile nu contine o valoare de tip intreg, se va afisa un mesaj de eroare in cadrul unei ferestre de tip toast.
-    // Fortati sistemul de operare Android sa nu salveze/restaureze starea campurilor text in cazul in care aceasta este distrusa. Implementati un mecanism de salvare/restaurare a starii activitatii in cazul in care aceasta este distrusa. Sa se afiseze aceste valori la pornirea activitatii in cadrul unei ferestre toast.
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(FIRST_NUMBER_EDIT_TEXT, firstNumberEditText.getText().toString());
+        outState.putString(SECOND_NUMBER_EDIT_TEXT, secondNumberEditText.getText().toString());
+        outState.putString(RESULT_TEXT_VIEW, resultTextView.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        firstNumber = savedInstanceState.getString(FIRST_NUMBER_EDIT_TEXT);
+        secondNumber = savedInstanceState.getString(SECOND_NUMBER_EDIT_TEXT);
+        result = savedInstanceState.getString(RESULT_TEXT_VIEW);
+
+        firstNumberEditText.setText(firstNumber);
+        secondNumberEditText.setText(secondNumber);
+        resultTextView.setText(result);
+        Toast.makeText(this, "First number: " + firstNumber + ", second number: " + secondNumber + ", result: " + result, Toast.LENGTH_SHORT).show();
+    }
 }
