e6d612b5863d49930cf66d49b0d588b79ce1f66f
Alex Necula <anecula@adobe.com>
2023-04-06 08:12:36 +0300

    Initial commit

---
+++ app/src/main/java/lab03/eim/systems/cs/pub/ro/practicaltest01var03/PracticalTest01Var03MainActivity.java
@@ -0,0 +1,14 @@
+package lab03.eim.systems.cs.pub.ro.practicaltest01var03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PracticalTest01Var03MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_var03);
+    }
+}
